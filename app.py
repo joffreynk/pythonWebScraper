777c22b8c6469a5bf19df4cb9e50717334f7bba7
Joffreynk <joyjoffrey@gmail.com>
2023-04-22 12:15:27 +0200

    retrieve web page from website

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
  
+ # Getting page HTML through request
+page = requests.get('https://people.com/celebrity/') 
+# Parsing content using beautifulsoup
+soup = BeautifulSoup(page.content, 'html.parser') 
+ 
+# Initialize List of celebraties
+celebraties = []
 
