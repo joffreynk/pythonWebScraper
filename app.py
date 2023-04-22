873f509c9f7d0a76cd182028b140b41efb762be7
Joffreynk <joyjoffrey@gmail.com>
2023-04-22 12:16:11 +0200

    get specific data from using soup

--- app.py
+++ app.py
@@ -9,3 +9,12 @@ soup = BeautifulSoup(page.content, 'html.parser')
 # Initialize List of celebraties
 celebraties = []
 
+file = open('celebraties')
+
+links = soup.select("#tax-sc__recirc-list_1-0 a") 
+for anchor in links:
+    text = anchor.select(' .card__content .card__title .card__title-text')[0].text
+    category = anchor.select(' .card__content')[0]['data-tag']
+    writer = anchor.select('.card__content .mntl-card__byline')[0]['data-byline']
+    imgURL = anchor.select('.loc.card__top .card__media.mntl-universal-image.card__media.universal-image__container .img-placeholder img')[0]['data-src']
+    
